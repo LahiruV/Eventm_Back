3d1f124cbe387d1df8da367103c7e2c2252135b2
DevshanL <devshankawshalyadkl@gmail.com>
2024-03-18 15:21:14 +0530

    model update

--- models/place.js
+++ models/place.js
@@ -16,7 +16,15 @@ const place = new Schema(
             type: Number,
             required: true,
         },
+        cost: {
+            type: Number,
+            required: true,
+        },
         description: {
+            type: String,
+            required: true,
+        },
+        image: {
             type: String,
             required: true,
         },
