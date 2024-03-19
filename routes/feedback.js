23dc5bd454ec461b6dcd4d2fc9cf638d2f45588a
lahir <lahir@NITRO5>
2024-03-19 23:15:11 +0530

    add

--- routes/feedback.js
+++ routes/feedback.js
@@ -15,25 +15,17 @@ router.route('/allfeedback').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/feedback/:email').get((req, res) => {
-    Feedback.findById(req.params.email)
-        .then(feedback => res.json(feedback))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
-
-router.route('/updatefeedback/:id').put((req, res) => {
-    Feedback.findById(req.params.id)
-        .then(feedback => {
-            feedback.feedbackId = req.body.feedbackId;
-            feedback.email = req.body.email;
-            feedback.description = req.body.description;
-            feedback.rating = req.body.rating;
-
-            feedback.save()
-                .then(() => res.json('Feedback updated successfully'))
-                .catch(err => res.status(400).json('Error: ' + err));
-        })
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/updatefeedback/").put(async (req, res) => {
+    const { feedbackId, email, description, rating } = req.body;
+    const feedback = {
+        feedbackId, email, description, rating
+    }
+    const update = await Feedback.findOneAndUpdate({ feedbackId: feedbackId }, feedback).then(() => {
+        res.status(200).send({ status: "Event FeedBack Updated" });
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({ status: "Error with Updating Data", error: err.message });
+    });
 });
 
 router.route('/deletefeedback/:feedbackId').delete((req, res) => {
